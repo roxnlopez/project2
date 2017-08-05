24a357e89fd1786e8512032d419ae38c3e464aa2
roxnlopez <roxnlopez@gmail.com>
2017-08-04 22:56:02 -0600

    added files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# project2
+TITLE
+*******************
+Trello: https://trello.com/b/86MX77gs/project-2
+*******************
+This is an app to save your movies. You can create lists to save your watched movies, want to see movies, movies to never watch.
---
+++ controllers/users.js
@@ -0,0 +1,60 @@
+var passport = require("passport");
+
+// GET /signup
+function getSignup(request, response, next) {
+  response.render('signup.ejs', {
+    message: request.flash('signupMessage')
+  });
+}
+
+// POST /signup
+function postSignup(request, response, next) {
+  //save a new user
+  let signupStrategy = passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
+  });
+  // console.log("YO");
+  return signupStrategy(request, response, next);
+}
+
+// GET /login
+function getLogin(request, response, next) {
+  response.render('login.ejs', {
+    message: request.flash('loginMessage')
+  });
+}
+
+// POST /login 
+function postLogin(request, response, next) {
+  //user login
+  let loginStrategy = passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  });
+  return loginStrategy(request, response, next);
+}
+
+// GET /logout
+function getLogout(request, response, next) {
+  request.logout();
+  response.redirect('/');
+}
+
+// Restricted page
+function secret(request, response) {
+  response.render('secret.ejs', {
+    message: request.flash('secretMessage')
+  });
+}
+
+module.exports = {
+  getLogin: getLogin,
+  postLogin: postLogin,
+  getSignup: getSignup,
+  postSignup: postSignup,
+  getLogout: getLogout,
+  secret: secret
+};
